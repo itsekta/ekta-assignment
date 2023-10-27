803f7cec089f111869077c2c4549978ea5cf943f
Ekta Chaudhary <ektachaudhary129@gmail.com>
2023-10-27 16:48:37 +0530

    WIP: landing page components

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css';
+function NavBar() {
+  return (
+    <div className="navbar-wrapper">
+      <div className="navbar-heading">
+        The <span className="navbar-product">Product</span> Platform
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
