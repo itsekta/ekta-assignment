c0ab46884c1ad45fa83521baa25e7c6be02d8f2d
Ekta Chaudhary <ektachaudhary129@gmail.com>
2023-10-29 00:13:57 +0530

    Refactored code

--- src/components/TimerSection.js
+++ src/components/TimerSection.js
@@ -2,59 +2,61 @@ import React, { useState, useEffect } from 'react';
 import './TimerSection.css';
 
 function TimerSection({ isTimerActive, initialHours, initialMinutes, initialSeconds, onTimerCompletion }) {
-  const [hours, setHours] = useState(initialHours);
-  const [minutes, setMinutes] = useState(initialMinutes);
-  const [seconds, setSeconds] = useState(initialSeconds);
-  const [timerEnded, setTimerEnded] = useState(false);
-  const [showTimer, setShowTimer] = useState(true);
-  const HideTimerSection = () => {
-    const timerCompleted = true;
-    onTimerCompletion(timerCompleted);
-  };
+  const [time, setTime] = useState({
+    hours: initialHours,
+    minutes: initialMinutes,
+    seconds: initialSeconds,
+  });
+
   useEffect(() => {
+    if (!isTimerActive) return;
+
     const timerInterval = setInterval(() => {
-      if (hours === 0 && minutes === 0 && seconds === 0) {
+      if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) {
         clearInterval(timerInterval);
-        setTimerEnded(true);
-        HideTimerSection();
-        if (minutes === 0) {
-          setHours(0);
-        }
+        onTimerCompletion(true);
       } else {
-        if (minutes === 0 && seconds === 0) {
-          setHours(prevHours => prevHours - 1);
-          setMinutes(59);
-          setSeconds(59);
-        } else if (seconds === 0) {
-          setMinutes(prevMinutes => prevMinutes - 1);
-          setSeconds(59);
+        const newTime = { ...time };
+
+        if (newTime.minutes === 0 && newTime.seconds === 0) {
+          newTime.hours -= 1;
+          newTime.minutes = 59;
+          newTime.seconds = 59;
+        } else if (newTime.seconds === 0) {
+          newTime.minutes -= 1;
+          newTime.seconds = 59;
         } else {
-          setSeconds(prevSeconds => prevSeconds - 1);
+          newTime.seconds -= 1;
         }
+
+        setTime(newTime);
       }
     }, 1000);
 
     return () => {
       clearInterval(timerInterval);
     };
-  }, [hours, minutes, seconds]);
+  }, [isTimerActive, time, onTimerCompletion]);
 
   return isTimerActive ? (
     <div className="timer-section-wrapper">
-      {hours > 0 && (
-        <div className="timer-hour">
-          <div className="timer-digits">{hours < 10 ? `0${hours}` : hours}</div>
-          <div className="timer-label">Hours</div>
-        </div>
+      {time.hours > 0 && (
+        <>
+          <div className="timer-hour">
+            <div className="timer-digits">{time.hours < 10 ? `0${time.hours}` : time.hours}</div>
+            <div className="timer-label">Hours</div>
+          </div>
+          <div>:</div>
+        </>
       )}
-      {hours > 0 && <div>:</div>}
+
       <div className="timer-minute">
-        <div className="timer-digits">{minutes < 10 ? `0${minutes}` : minutes}</div>
+        <div className="timer-digits">{time.minutes < 10 ? `0${time.minutes}` : time.minutes}</div>
         <div className="timer-label">Minutes</div>
       </div>
       <div>:</div>
       <div className="timer-seconds">
-        <div className="timer-digits">{seconds < 10 ? `0${seconds}` : seconds}</div>
+        <div className="timer-digits">{time.seconds < 10 ? `0${time.seconds}` : time.seconds}</div>
         <div className="timer-label">Seconds</div>
       </div>
     </div>
