0361a07f2ebfd1c9b7cc8d3e4feef100fbdea573
Ekta Chaudhary <ektachaudhary129@gmail.com>
2023-10-28 22:47:54 +0530

    Fixed: blob animation and notification UI

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -4,39 +4,46 @@ import CircularProgress from '@mui/material/CircularProgress';
 import CheckIcon from '@mui/icons-material/Check';
 
 function NotificationSection() {
-  const inputRef = useRef(null);
+  const inputRef = useRef('');
+  const [inputValue, setInputValue] = useState('');
   const [isValidEmail, setIsValidEmail] = useState(false);
-  const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+  const [isInvalidEmail, setIsInvalidEmail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isDone, setIsDone] = useState(false);
-
+  const handleInputChange = event => {
+    setInputValue(event.target.value);
+  };
   const validateEmail = email => {
-    let regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    let regex = /^[A-Z0-9._%+-]+@gmail\.com$/i && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     const isValid = regex.test(email);
     setIsValidEmail(isValid);
-    setIsInvalidEmail(!isValid);
-    console.log(isValid);
   };
   const handleNotifyClick = () => {
+    validateEmail(inputValue);
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
     }, 3000);
     if (!isValidEmail) {
-      setIsInvalidEmail(true);
+      setTimeout(() => {
+        setIsInvalidEmail(true);
+        setIsDone(false);
+      }, 3000);
+    } else {
+      setIsInvalidEmail(false);
+      setIsDone(true);
     }
-    setIsDone(true);
   };
   return (
     <div className="notification-wrapper">
       <div className="notification-desc">{!isLoading && isDone ? `You're all set! You'll be the first to know when we're live.` : `Be the first to know! Share your email and We'll notify you when it's live`}</div>
       <div className="notification-action">
-        <input className="notification-input" placeholder="Please enter your email id" type="email" ref={inputRef} onChange={() => validateEmail(inputRef.current.value)}></input>
-        <button className={`notification-button ${isValidEmail ? '' : 'disabled'}`} onClick={handleNotifyClick} disabled={isLoading || !isValidEmail || isDone}>
+        <input className="notification-input" placeholder="Please enter your email id" type="email" value={inputValue} onChange={handleInputChange}></input>
+        <button className={`notification-button ${inputValue === '' ? 'disabled' : ''}`} onClick={handleNotifyClick} disabled={isLoading || isDone}>
           {isLoading ? <CircularProgress size="24px" className="circular-progress" /> : isDone ? <CheckIcon fontSize="32px" /> : 'Notify me'}{' '}
         </button>
-        <div className="invalid-email"> {isInvalidEmail && isLoading ? 'Invalid email' : ''}</div>
       </div>
+      <div className="invalid-email"> {isInvalidEmail ? 'Invalid email' : ''}</div>
     </div>
   );
 }
