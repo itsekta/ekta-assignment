0b6893f332098e61a465376180e0027a6ee60f00
Ekta Chaudhary <ektachaudhary129@gmail.com>
2023-10-28 23:54:05 +0530

    Added: timer settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,36 @@
 'use client';
 import './page.css';
+import { useState } from 'react';
 import NavBar from '@/components/NavBar';
 import HeaderSection from '@/components/HeaderSection';
 import TimerSection from '@/components/TimerSection';
 import NotificationSection from '@/components/NotificationSection';
 import BlobAnimation from '@/components/BlobAnimation';
 import PageSection from '@/components/PageSection';
+import TimerPopup from '@/components/TimerPopup';
 export default function Home() {
+  const [childData, setChildData] = useState('');
+  const [showAnotherComponent, setShowAnotherComponent] = useState(false);
+
+  const handleChildData = data => {
+    setShowAnotherComponent(data);
+  };
   return (
     <main className="min-h-screen main">
       <BlobAnimation />
       <NavBar />
       <PageSection>
         <div className="main-section-wrapper">
-          <div className="main-section">
-            <HeaderSection />
-            <div className="get-ready-text">Get Ready for the reveal!</div>
-            <TimerSection />
-            <NotificationSection />
-          </div>
+          {!showAnotherComponent && (
+            <div className="main-section">
+              <HeaderSection />
+              <div className="get-ready-text">Get Ready for the reveal!</div>
+              <TimerSection isTimerActive={true} initialHours={0} initialMinutes={0} initialSeconds={5} onTimerCompletion={handleChildData} />
+              {showAnotherComponent}
+              <NotificationSection />
+            </div>
+          )}
+          {showAnotherComponent ? <TimerPopup /> : null}
         </div>
       </PageSection>
     </main>
--- src/components/TimerSection.js
+++ src/components/TimerSection.js
@@ -1,23 +1,64 @@
+import React, { useState, useEffect } from 'react';
 import './TimerSection.css';
 
-function TimerSection() {
-  return (
+function TimerSection({ isTimerActive, initialHours, initialMinutes, initialSeconds, onTimerCompletion }) {
+  const [hours, setHours] = useState(initialHours);
+  const [minutes, setMinutes] = useState(initialMinutes);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [timerEnded, setTimerEnded] = useState(false);
+  const [showTimer, setShowTimer] = useState(true);
+  const HideTimerSection = () => {
+    const timerCompleted = true;
+    onTimerCompletion(timerCompleted);
+  };
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      if (hours === 0 && minutes === 0 && seconds === 0) {
+        clearInterval(timerInterval);
+        setTimerEnded(true);
+        HideTimerSection();
+        if (minutes === 0) {
+          setHours(0);
+        }
+      } else {
+        if (minutes === 0 && seconds === 0) {
+          setHours(prevHours => prevHours - 1);
+          setMinutes(59);
+          setSeconds(59);
+        } else if (seconds === 0) {
+          setMinutes(prevMinutes => prevMinutes - 1);
+          setSeconds(59);
+        } else {
+          setSeconds(prevSeconds => prevSeconds - 1);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [hours, minutes, seconds]);
+
+  return isTimerActive ? (
     <div className="timer-section-wrapper">
-      <div className="timer-hour">
-        <div className="timer-digits">00</div>
-        <div className="timer-label">Hours</div>
-      </div>
-      <div>:</div>
+      {hours > 0 && (
+        <div className="timer-hour">
+          <div className="timer-digits">{hours < 10 ? `0${hours}` : hours}</div>
+          <div className="timer-label">Hours</div>
+        </div>
+      )}
+      {hours > 0 && <div>:</div>}
       <div className="timer-minute">
-        <div className="timer-digits">00</div>
+        <div className="timer-digits">{minutes < 10 ? `0${minutes}` : minutes}</div>
         <div className="timer-label">Minutes</div>
       </div>
       <div>:</div>
       <div className="timer-seconds">
-        <div className="timer-digits">00</div>
+        <div className="timer-digits">{seconds < 10 ? `0${seconds}` : seconds}</div>
         <div className="timer-label">Seconds</div>
       </div>
     </div>
-  );
+  ) : null;
 }
+
 export default TimerSection;
