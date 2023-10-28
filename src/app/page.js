9af5963efded568376caa40cde43c66b0f5cb3b7
Ekta Chaudhary <ektachaudhary129@gmail.com>
2023-10-28 19:31:58 +0530

    Added: email validation

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import './page.css';
 import NavBar from '@/components/NavBar';
 import HeaderSection from '@/components/HeaderSection';
--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -1,12 +1,18 @@
+import { useRef, useState } from 'react';
 import './NotificationSection.css';
-
 function NotificationSection() {
+  const inputRef = useRef(null);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const validateEmail = email => {
+    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    setIsValidEmail(regex.test(email));
+  };
   return (
     <div className="notification-wrapper">
       <div className="notification-desc">Be the first to know! Share your email and We'll notify you when it's live</div>
       <div className="notification-action">
-        <input className="notification-input" placeholder="Please enter your email id" type="email"></input>
-        <button className="notification-button">Notify me</button>
+        <input className="notification-input" placeholder="Please enter your email id" type="email" ref={inputRef} onChange={() => validateEmail(inputRef.current.value)}></input>
+        <button className={`notification-button ${isValidEmail ? '' : 'disabled'}`}>Notify me</button>
       </div>
     </div>
   );
